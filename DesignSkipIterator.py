caa903ee27e8909838c5342ecc3f14d1664f7d1f
Gautam-Balachandran <gautam.balachandran@gmail.com>
2024-07-06 16:28:05 -0400

    Design-4 Completed

---
+++ DesignSkipIterator.py
@@ -0,0 +1,84 @@
+﻿# Time Complexity : O(1)
+# Space Complexity : O(n), where n is the number of elements in the input iterator
+
+class SkipIterator:
+    def __init__(self, iterator):
+        self.it = iterator
+        self.skip_map = {}
+        self.next_el = None
+        self.advance()
+
+    def advance(self):
+        self.next_el = None
+        while self.next_el is None:
+            try:
+                el = next(self.it)
+            except StopIteration:
+                break
+            if el in self.skip_map:
+                self.skip_map[el] -= 1
+                if self.skip_map[el] == 0:
+                    del self.skip_map[el]
+            else:
+                self.next_el = el
+
+    def has_next(self):
+        return self.next_el is not None
+
+    def next(self):
+        if self.next_el is None:
+            raise StopIteration()
+        result = self.next_el
+        self.advance()
+        return result
+
+    def skip(self, val):
+        if self.next_el == val:
+            self.advance()
+        else:
+            if val in self.skip_map:
+                self.skip_map[val] += 1
+            else:
+                self.skip_map[val] = 1
+
+# Example 1
+iterator = iter([2, 3, 5, 6, 5, 7, 5, -1, 5, 10])
+skip_itr = SkipIterator(iterator)
+
+print(skip_itr.has_next())  # True
+print(skip_itr.next())      # 2
+skip_itr.skip(5)
+print(skip_itr.next())      # 3
+print(skip_itr.next())      # 6 (skips first 5)
+print(skip_itr.next())      # 5
+skip_itr.skip(5)
+skip_itr.skip(5)
+print(skip_itr.next())      # 7
+print(skip_itr.next())      # -1
+print(skip_itr.next())      # 10
+print(skip_itr.has_next())  # False
+try:
+    print(skip_itr.next())  # Error
+except StopIteration:
+    print("StopIteration")  # Expected output
+
+# Example 2
+iterator = iter([1, 2, 3, 4, 5])
+skip_itr = SkipIterator(iterator)
+
+print(skip_itr.next())      # 1
+skip_itr.skip(2)
+print(skip_itr.next())      # 3 (skips 2)
+print(skip_itr.has_next())  # True
+print(skip_itr.next())      # 4
+
+# Example 3
+iterator = iter([1, 2, 2, 3, 4])
+skip_itr = SkipIterator(iterator)
+
+skip_itr.skip(2)
+print(skip_itr.next())      # 1
+print(skip_itr.next())      # 2 (first 2 is skipped)
+skip_itr.skip(3)
+print(skip_itr.next())      # 4 (skips 3)
+print(skip_itr.has_next())  # False
---
+++ DesignTwitter.py
@@ -0,0 +1,71 @@
+﻿import heapq
+from collections import defaultdict, deque
+
+# Space Complexity : O(n+m), where n is the number of users and m is the number of tweets
+class Twitter:
+    class Tweet:
+        def __init__(self, tweet_id, created_at):
+            self.tweet_id = tweet_id
+            self.created_at = created_at
+
+    def __init__(self):
+        self.user_map = defaultdict(set)
+        self.tweet_map = defaultdict(deque)
+        self.time = 0
+
+    # Time Complexity : O(1)
+    def postTweet(self, userId, tweetId):
+        self.tweet_map[userId].appendleft(self.Tweet(tweetId, self.time))
+        self.time += 1
+
+    # Time Complexity : O(n+m)
+    def getNewsFeed(self, userId):
+        self.follow(userId, userId)
+        min_heap = []
+        all_users = self.user_map[userId]
+        for user in all_users:
+            for tweet in self.tweet_map[user]:
+                heapq.heappush(min_heap, (tweet.created_at, tweet.tweet_id))
+                if len(min_heap) > 10:
+                    heapq.heappop(min_heap)
+        result = []
+        while min_heap:
+            result.append(heapq.heappop(min_heap)[1])
+        return result[::-1]
+
+    # Time Complexity : O(1)
+    def follow(self, followerId, followeeId):
+        self.user_map[followerId].add(followeeId)
+
+    # Time Complexity : O(1)
+    def unfollow(self, followerId, followeeId):
+        if followeeId in self.user_map[followerId]:
+            self.user_map[followerId].remove(followeeId)
+
+# Example 1
+twitter = Twitter()
+twitter.postTweet(1, 5)
+print(twitter.getNewsFeed(1))  # Output: [5]
+
+# Example 2
+twitter.postTweet(1, 3)
+twitter.postTweet(1, 101)
+twitter.postTweet(1, 13)
+twitter.postTweet(1, 10)
+twitter.postTweet(1, 2)
+twitter.postTweet(1, 94)
+twitter.postTweet(1, 505)
+twitter.postTweet(1, 333)
+twitter.postTweet(1, 22)
+twitter.postTweet(1, 11)
+print(twitter.getNewsFeed(1))  # Output: [11, 22, 333, 505, 94, 2, 10, 13, 101, 3]
+
+# Example 3
+twitter.follow(2, 1)
+print(twitter.getNewsFeed(2))  # Output: [11, 22, 333, 505, 94, 2, 10, 13, 101, 3]
+twitter.postTweet(2, 6)
+print(twitter.getNewsFeed(2))  # Output: [6, 11, 22, 333, 505, 94, 2, 10, 13, 101]
+
+# Example 4 (unfollowing)
+twitter.unfollow(2, 1)
+print(twitter.getNewsFeed(2))  # Output: [6]
